779ced4e5facc36da6787b2b275b59ac0d500120
vissul <gvissul@gmail.com>
2013-11-12 11:19:18 +0800

    updated

--- code/henanqixiang/src/main/java/tianci/qixiang/henan/DistributionController.java
+++ code/henanqixiang/src/main/java/tianci/qixiang/henan/DistributionController.java
@@ -6,25 +6,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value="/distribution")
 public class DistributionController {
 	
-	@RequestMapping(value="/", method = RequestMethod.GET)
+	@RequestMapping(value="/distribution", method = RequestMethod.GET)
 	public String index(Model model){
 		
 		return "distribution/index";
 	}
-	@RequestMapping(value="/current", method = RequestMethod.GET)
+	@RequestMapping(value="/distribution/current", method = RequestMethod.GET)
 	public String current(){
 		
 		return "distribution/current";
 	}
 	
-	@RequestMapping(value="/config", method = RequestMethod.GET)
+	@RequestMapping(value="/distribution/config", method = RequestMethod.GET)
 	public String config(){
 		return "distribution/config";
 	}
-	@RequestMapping(value="/monitor", method = RequestMethod.GET)
+	@RequestMapping(value="/distribution/monitor", method = RequestMethod.GET)
 	public String monitor(Model model){
 		
 		return "distribution/monitor";
